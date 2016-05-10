137ee51be3223ea67fcb2daaf335e5db0b552802
unknown <ggriffin@DEVGGRIFFIN.vistaprint.net>
2016-05-10 14:31:18 -0400

    Add logo.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import AppBar from 'material-ui/AppBar';
 const NavBar = () => (
   <AppBar
     style={{backgroundColor:'#3587A2'}}
-    title="noteworthy"
+    title={<img style={{height: '100%', paddingTop: '.3em'}} src={require('../assets/noteworthylogo.png')}></img>}
   />
 );
 
